02b8a1e582c75ce587d13cd680a07dfc01e54233
carmodagama <carmodagama@gmail.com>
2026-04-07 18:50:22 +0100

    feat(backend): RoleGuard + JWT strategy + ownership checks

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ServicesModule } from '../services/services.module';
+import { TransactionsModule } from '../transactions/transactions.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, UsersModule, ServicesModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
